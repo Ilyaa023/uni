8f16209829e35aa4b509de68dce308495c0bcb24
Ilyaa023 <96044968+Ilyaa023@users.noreply.github.com>
2024-03-02 11:46:46 +0500

    initial commit
    stock version

---
+++ RealTimeDeviceSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealTimeDeviceSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FireDetector> fdList = new List<FireDetector>();
+            for (int i=0; i<10; i++)
+            {
+                FireDetector fd = new FireDetector();
+                fd.FillFireDetector($"Датчик#{i}", "", "ДП1", 1, 1, i);
+                fdList.Add(fd);
+            }
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(2);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                foreach(var fd in fdList)
+                {
+                    fd.ChangeFireDetectorState();
+                    Console.WriteLine($"Состояние датчика {fd.Name} - Статус: {fd.Status}, Дым - {fd.SmokeStatus}");
+                }
+            }, null, startTimeSpan, periodTimeSpan);
+
+            Console.ReadKey();
+        }
+    }
+}
