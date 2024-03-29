f5ff593bf7b5026b494de487414dd165aefe879e
Ilyaa023 <96044968+Ilyaa023@users.noreply.github.com>
2024-03-29 14:41:23 +0500

    ex2

---
+++ RTDC/Controllers/VelocimeterController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RTDC.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class VelocimeterController : ControllerBase
+    {
+        List<Velocimeter> velocimeters = new List<Velocimeter>();
+        public VelocimeterController()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Velocimeter v = new Velocimeter();
+                v.FillVelocimeter($"Sensor#{i}", "", "DVA", 1, 1, i, 10, 15, 1);
+                velocimeters.Add(v);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(velocimeters);
+        }
+
+        [HttpGet]
+        public IActionResult GetDetectorByLocation([FromQuery] int buildingNumber, [FromQuery] int device, [FromQuery] int order)
+        {
+            Velocimeter v = velocimeters.FirstOrDefault(f => f.Location.BuildingNumber == buildingNumber &&
+                                                        f.Location.Order == order && f.Location.Device == device);
+            if (v == null)
+            {
+                return NotFound();
+            }
+            return Ok(v);
+        }
+
+        [HttpPost]
+        public IActionResult AddSensor([FromBody] Velocimeter meter)
+        {
+            Velocimeter v = velocimeters.FirstOrDefault(f => f.Location.BuildingNumber == meter.Location.BuildingNumber &&
+                                                            f.Location.Order == meter.Location.Order &&
+                                                            f.Location.Device == meter.Location.Device);
+            velocimeters.Add(v);
+            return Created("", meter);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateSensor([FromBody] Velocimeter meter)
+        {
+            Velocimeter v = velocimeters.FirstOrDefault(f => f.Location.BuildingNumber == meter.Location.BuildingNumber &&
+                                                            f.Location.Order == meter.Location.Order &&
+                                                            f.Location.Device == meter.Location.Device);
+            if (v == null)
+            {
+                return BadRequest();
+            }
+            v.Name= meter.Name;
+            v.Description= meter.Description;
+            v.Model= meter.Model;
+            v.VelocityValue = meter.VelocityValue;
+            v.Status= meter.Status;
+            v.WarningSetpointHigh= meter.WarningSetpointHigh;
+            v.EmergencySetpointHigh= meter.EmergencySetpointHigh;
+            v.Operability= meter.Operability;
+            return Ok(v);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteSensor([FromBody] Velocimeter meter)
+        {
+            Velocimeter v = velocimeters.FirstOrDefault(f => f.Location.BuildingNumber == meter.Location.BuildingNumber &&
+                                                            f.Location.Order == meter.Location.Order &&
+                                                            f.Location.Device == meter.Location.Device);
+            velocimeters = velocimeters.Where(f => !(f.Location.BuildingNumber == meter.Location.BuildingNumber &&
+                                                            f.Location.Order == meter.Location.Order &&
+                                                            f.Location.Device == meter.Location.Device)).ToList();
+            return Ok(velocimeters);
+        }
+    }
+}
---
+++ RTDC/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTDC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*List<Velocimeter> vList = new List<Velocimeter>();
+            for (int i = 0; i < 10; i++)
+            {
+                Velocimeter v = new Velocimeter();
+                v.FillVelocimeter($"Sensor#{i}", "", "DVA", 1, 1, i, 10, 15, 1);
+                vList.Add(v);
+            }
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(2);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                foreach (var v in vList)
+                {
+                    v.ChangeFireDetectorState(1);
+                    Console.WriteLine($"Status of {v.Name} is: {v.Status}, Velocity = {v.VelocityValue}");
+                }
+                Console.WriteLine();
+            }, null, startTimeSpan, periodTimeSpan);*/
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
